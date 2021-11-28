a24e45eca161d05a87bbf412b50a2e3f9bea14bb
Jimmyis <me@jimmyis.com>
2021-11-29 04:28:36 +0700

    Updated background color the Global Page component.

--- src/pages/GlobalPage.js
+++ src/pages/GlobalPage.js
@@ -65,7 +65,7 @@ function GlobalPage() {
 
   return (
     <PageWrapper>
-      <ThemedBackground backgroundColor={transparentize(0.8, '#D45D02')} />
+      <ThemedBackground backgroundColor={transparentize(0.8, '#00ABFF')} />
       <ContentWrapper>
         <div>
           <AutoColumn gap="24px" style={{ paddingBottom: below800 ? '0' : '24px' }}>
